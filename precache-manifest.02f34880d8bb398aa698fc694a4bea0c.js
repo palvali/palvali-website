3e6896848f2e15d6d2adc0a1260f8ddb497f7777
Teja <palvali.teja@gmail.com>
2019-10-15 13:13:45 -0700

    Updates

---
+++ precache-manifest.02f34880d8bb398aa698fc694a4bea0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5046401267f7b17018cc6e6c5176f399",
+    "url": "/palvali-website/index.html"
+  },
+  {
+    "revision": "7e5d8a0776d549604c9d",
+    "url": "/palvali-website/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4dc4e30036b21cd72f3e",
+    "url": "/palvali-website/static/css/main.1f9002d1.chunk.css"
+  },
+  {
+    "revision": "7e5d8a0776d549604c9d",
+    "url": "/palvali-website/static/js/2.ada6cf30.chunk.js"
+  },
+  {
+    "revision": "4dc4e30036b21cd72f3e",
+    "url": "/palvali-website/static/js/main.35f2518e.chunk.js"
+  },
+  {
+    "revision": "38ff86b4182654042235",
+    "url": "/palvali-website/static/js/runtime~main.793c6f62.js"
+  }
+]);
